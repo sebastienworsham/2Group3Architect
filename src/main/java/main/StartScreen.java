ebcd44fa8c28f9e3c2d43f59e618b636107eb895
Nishtha Sharma <158241584+nishthasharma0@users.noreply.github.com>
2024-03-04 20:39:21 -0800

    Update StartScreen

--- src/main/java/main/StartScreen.java
+++ src/main/java/main/StartScreen.java
@@ -1,19 +1,17 @@
 package main;
 
 import entities.Player;
-import javafx.animation.AnimationTimer;
-import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.input.KeyCode;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
+import javafx.scene.control.Label;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
-
-import java.util.HashMap;
-
+import static java.awt.SystemColor.text;
 import static main.Main.GAMEHEIGHT;
 import static main.Main.GAMEWIDTH;
 
@@ -33,19 +31,63 @@ public class StartScreen {
     }
 
     void renderStartScreen() {
-        uiRoot.setStyle("-fx-background-color: black;");
+        uiRoot.setStyle("-fx-background-color: #87ce87;");
         uiRoot.setPrefSize(GAMEWIDTH, GAMEHEIGHT);
+
+        Label gameName = new Label("ROLLING  RIFT");
+        gameName.setFont(Font.font("Game Over", FontWeight.BOLD, 200));
+        gameName.setAlignment(Pos.TOP_CENTER);
+        gameName.setStyle("-fx-text-fill: black;");
+
 
         Button newGameButton = new Button("New Game");
         Button loadGameButton = new Button("Load Game");
-        newGameButton.setOnAction(event -> {
-            setupGameScene();
+        newGameButton.setFont(Font.font("Gameplay", FontWeight.BOLD, 20));
+        loadGameButton.setFont(Font.font("Gameplay", FontWeight.BOLD, 19));
+
+        newGameButton.setOnMouseMoved(event -> {
+                newGameButton.setStyle("-fx-background-color: #78ced9; ");
+        });
+        newGameButton.setOnMouseExited(event -> {
+                newGameButton.setStyle("-fx-background-color: #ffffff; ");
+        });
+        loadGameButton.setOnMouseMoved(event -> {
+                loadGameButton.setStyle("-fx-background-color: #78ced9");
+        });
+        loadGameButton.setOnMouseExited(event -> {
+                loadGameButton.setStyle("-fx-background-color: #ffffff; ");
         });
 
-        VBox vbox = new VBox(10);
-        vbox.setAlignment(Pos.CENTER);
-        vbox.getChildren().addAll(newGameButton, loadGameButton);
-        uiRoot.getChildren().add(vbox);
+
+        newGameButton.setOnAction(event -> {
+                setupGameScene();
+        });
+
+
+        HBox hBox = new HBox();
+        hBox.setSpacing(20);
+
+        hBox.getChildren().addAll(newGameButton, loadGameButton);
+        hBox.setAlignment(Pos.CENTER);
+
+
+        HBox topBox = new HBox();
+        topBox.setFillHeight(true);
+        topBox.getChildren().add(gameName);
+        topBox.setAlignment(Pos.CENTER);
+
+        topBox.setMargin(gameName, new Insets(300, 0, 0, 0));
+        BorderPane layout = new BorderPane();
+
+        layout.setPrefSize(GAMEWIDTH, GAMEHEIGHT);
+        layout.setTop(topBox);
+        layout.setCenter(hBox);
+
+        HBox bottomBox = new HBox();
+        topBox.setMaxHeight(100);
+
+        layout.setBottom(bottomBox);
+        uiRoot.getChildren().add(layout);
     }
 
     private void setupGameScene() {
