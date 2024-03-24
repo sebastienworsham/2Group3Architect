153831904f0a8446494c3bb38ab2a551158864a7
Nishtha Sharma <158241584+nishthasharma0@users.noreply.github.com>
2024-03-23 17:48:02 -0700

    Move the location of StartScreenTest.java

---
+++ src/test/java/main/LevelScreenTest.java
@@ -0,0 +1,20 @@
+package main;
+
+import java.util.List;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import main.StartScreen;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.testfx.framework.junit5.ApplicationTest;
+
+class LevelScreenTest extends ApplicationTest {
+
+}
--- src/test/java/main/StartScreenTest.java
+++ src/test/java/main/StartScreenTest.java
@@ -1,9 +1,3 @@
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by FernFlower decompiler)
-//
-
-
 import java.util.List;
 import javafx.scene.Node;
 import javafx.scene.Parent;
