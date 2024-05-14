bd8e12ecbf2b9084fa685ab483dae2ea2d3ddc85
thumb <thumbho2018@gmail.com>
2024-05-14 14:12:15 -0700

    searching and sorting test

---
+++ src/test/java/main/SearchingAndSortingTest.java
@@ -0,0 +1,21 @@
+package main;
+
+import org.junit.jupiter.api.Test;
+import main.*;
+import static org.junit.Assert.assertEquals;
+
+public class SearchingAndSortingTest {
+    String playerName = "User1";
+    User user = new User("User1", 0);
+
+    @Test
+    public void testgetUserLevelByLinearsearch() {
+        String playerName = "User1";
+        assertEquals(user.getUserLevelByLinearSearch(playerName), 0);
+    }
+    @Test
+    public void testgetPlayerInformation() {
+        String playerName = "User1";
+        assertEquals(user.getPlayerInformation(playerName), "User1");
+    }
+}
