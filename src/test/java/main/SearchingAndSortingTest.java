54a57c3b0ee4c266fe2feaf1656ae255c10163ad
thumb <thumbho2018@gmail.com>
2024-05-15 22:49:59 -0700

    searching and sorting test done

--- src/test/java/main/SearchingAndSortingTest.java
+++ src/test/java/main/SearchingAndSortingTest.java
@@ -5,7 +5,7 @@ import javafx.stage.Stage;
 import org.junit.jupiter.api.Test;
 import main.LoadScreen;
 import static org.junit.Assert.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 public class SearchingAndSortingTest {
     String playerName = "User1";
@@ -19,10 +19,10 @@ public class SearchingAndSortingTest {
         assertEquals(result, "0");
     }
     @Test
+
     public void testgetPlayerInformation() {
         LoadScreen loadScreen = new LoadScreen(null, null, null, null);
         String playerName = "User1";
-        assertEquals(loadScreen.getPlayerInformation(playerName), "User1");
         String[] result = loadScreen.getPlayerInformation(playerName);
         String[] expected = {"User1", "0"}; // Assuming "User1" and "0" are the expected results
         assertArrayEquals(expected, result);
